b5205ef26400ef2d71114ca62aeb030fa02250fd
CaioMoussa <caioma@usp.br>
2022-06-02 11:26:24 -0300

    Botei umas string

--- lerolero.py
+++ lerolero.py
@@ -1,3 +1,20 @@
 #asdasdasd
 print("HelloJujuba")
 
+parte1 = [
+"QUalquer cosia1"
+"Frase de efeito2"
+"Frese de efeito 3"
+]
+
+parte2 = [
+"sla1"
+"sla21"
+"papai234"
+]
+
+parte3[
+"AAAAAAAA"
+"BBBBBBBBBBBB"
+"Caio"
+]
